9535b535a93956f5ce1342a099c3229170675d09
Harald Kirschner <hkirschner@mozilla.com>
2015-06-01 14:26:49 -0700

    Fixed assets

--- script/Main.js
+++ script/Main.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     paths: {
 
-      // base: "http://mozilla.github.io/devtools-perf-game/"
+      base: "http://mozilla.github.io/devtools-perf-game/"
 
     },
 
